ecb6c6f0923f61ce0aa237124573008d7f700c15
admin <admin@10.10.0.190>
2016-05-09 10:30:41 +0300

    añadida linea

--- ParqueAtracciones/src/Main.java
+++ ParqueAtracciones/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Probando github");
 		//Comentario de prueba commit
+		System.out.println("Salvando al repositorio local");
 	}
 }
