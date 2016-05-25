832f0cc0961dcca77758b459e5fe1c976eace4ae
admin <admin@10.10.0.191>
2016-05-25 15:39:10 +0300

    dia 25 mayo

--- ParqueAtracciones/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-	public static void main(String[] args){
-		System.out.println("Probando github");
-		//Comentario de prueba commit
-		System.out.println("Salvando al repositorio local");
-	}
-}
---
+++ ParqueAtracciones/src/parque/parque.java
@@ -0,0 +1,9 @@
+package parque;
+
+public class parque {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
